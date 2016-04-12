9ff3f24a067ff601fc60d867e715fa6e339e4995
Wim Looman <wim@nemo157.com>
2016-04-12 22:05:00 +0200

    Add hamlet test bypassing std::fmt

--- benches/hamlet.rs
+++ benches/hamlet.rs
@@ -4,14 +4,15 @@
 extern crate hamlet;
 extern crate test;
 
+use hamlet::Token;
 use test::{ Bencher, black_box };
 
 fn run(mut s: String) -> String {
     use std::fmt::Write;
     let events = [
-        hamlet::Token::start_tag("div", attrs!()),
-        hamlet::Token::text("Hello world!"),
-        hamlet::Token::end_tag("div"),
+        Token::start_tag("div", attrs!()),
+        Token::text("Hello world!"),
+        Token::end_tag("div"),
     ];
     for event in &events {
         write!(s, "{}", event).unwrap();
@@ -19,15 +20,60 @@ fn run(mut s: String) -> String {
     s
 }
 
-fn prepare() -> [hamlet::Token<'static>; 3] {
+fn run_no_fmt(mut s: String) -> String {
+    struct T<'a, 'b: 'a>(&'a Token<'b>);
+
+    impl<'a, 'b> T<'a, 'b> {
+        fn write_to(&self, s: &mut String) {
+            match *self.0 {
+                Token::StartTag { ref name, ref attrs, self_closing } => {
+                    s.push('<');
+                    s.push_str(name);
+                    for _ in attrs.iter() {
+                        unimplemented!();
+                    }
+                    if self_closing {
+                        s.push_str(" />")
+                    } else {
+                        s.push('>')
+                    }
+                }
+                Token::EndTag { ref name } => {
+                    s.push_str("</");
+                    s.push_str(name);
+                    s.push('>');
+                }
+                Token::Text(ref text) => {
+                    // No escaping...
+                    s.push_str(text);
+                }
+                Token::RawText(_) => unimplemented!(),
+                Token::Comment(_) => unimplemented!(),
+                Token::DOCTYPE => unimplemented!(),
+            }
+        }
+    }
+
+    let events = [
+        Token::start_tag("div", attrs!()),
+        Token::text("Hello world!"),
+        Token::end_tag("div"),
+    ];
+    for event in &events {
+        T(event).write_to(&mut s);
+    }
+    s
+}
+
+fn prepare() -> [Token<'static>; 3] {
     [
-        hamlet::Token::start_tag("div", attrs!()),
-        hamlet::Token::text("Hello world!"),
-        hamlet::Token::end_tag("div"),
+        Token::start_tag("div", attrs!()),
+        Token::text("Hello world!"),
+        Token::end_tag("div"),
     ]
 }
 
-fn run_prepared(events: &[hamlet::Token<'static>; 3], mut s: String) -> String {
+fn run_prepared(events: &[Token<'static>; 3], mut s: String) -> String {
     use std::fmt::Write;
     for event in events {
         write!(s, "{}", event).unwrap();
@@ -43,6 +89,13 @@ fn test() {
 }
 
 #[test]
+fn test_no_fmt() {
+    assert_eq!(
+        run_no_fmt(String::new()),
+        "<div>Hello world!</div>");
+}
+
+#[test]
 fn test_prepared() {
     let events = prepare();
     assert_eq!(
@@ -53,6 +106,11 @@ fn test_prepared() {
 #[bench]
 fn bench(b: &mut Bencher) {
     b.iter(|| run(black_box(String::new())))
+}
+
+#[bench]
+fn bench_no_fmt(b: &mut Bencher) {
+    b.iter(|| run_no_fmt(black_box(String::new())))
 }
 
 #[bench]
